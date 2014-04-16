80a87f0c4e65a928163f9158ba802e07ec9376eb
Nimit Maru <nimit@yealthy.com>
2014-04-16 01:54:24 -0400

    adding standalone jasmine testing for selector

---
+++ src/selector.js
@@ -0,0 +1,23 @@
+var traverseDom = function(startEl, matchFunc) {
+  var resultSet = [];
+  
+  if (typeof startEl == "undefined") {
+    startEl = document.body;
+  }
+
+  // your code here
+  // your code here
+  // your code here
+
+  return resultSet;
+};
+
+var $ = function (selector) {
+  var elements;
+  
+  // your code here
+  // your code here
+  // your code here
+  
+  return elements;
+};
